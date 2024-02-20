3e438940820f2e9e5dd53490aa8f08d4ed419c48
XXXDESHXXX <kirya.shvarts@list.ru>
2024-02-20 11:32:56 +0300

    Create admin playlists panel

--- playlists/admin.py
+++ playlists/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Playlist, PlaylistSong
 
-# Register your models here.
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ['name', 'image', 'created_at',
+                    'is_private', 'description', 'user']
+    list_filter = ['created_at', 'updated_at', 'is_private']
+    search_fields = ['name', 'description']
+
+
+@admin.register(PlaylistSong)
+class PlaylistSongAdmin(admin.ModelAdmin):
+    list_display = ['created_at', 'song']
+    list_filter = ['created_at']
