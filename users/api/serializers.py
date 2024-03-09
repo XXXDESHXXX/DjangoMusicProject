e94620588de885224ac0f369955574a297255bff
XXXDESHXXX <kirya.shvarts@list.ru>
2024-03-09 22:24:28 +0300

    Create CreateFollow and DeleteFollow views; Create serializers for them; Create urls.py

---
+++ users/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from users.models import UserFollow
+
+
+class UserFollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserFollow
+        fields = ('id', 'user_from', 'user_to', 'created')
+        read_only_fields = ('created',)
+
+
+class UserFollowCreateSerializer(UserFollowSerializer):
+
+    class Meta(UserFollowSerializer.Meta):
+        read_only_fields = UserFollowSerializer.Meta.read_only_fields + ('user_from',)
---
+++ users/api/views.py
@@ -0,0 +1,34 @@
+# views.py
+
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.generics import DestroyAPIView, CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+
+from users.models import UserFollow
+from .serializers import UserFollowSerializer, UserFollowCreateSerializer
+
+User = get_user_model()
+
+
+class UserFollowCreateAPIView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserFollowCreateSerializer
+
+    def perform_create(self, serializer: Serializer) -> None:
+        serializer.save(user_from=self.request.user)
+
+
+class UserFollowDeleteAPIView(DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self) -> UserFollow:
+        user_id = self.kwargs.get("user_id")
+        user_to = get_object_or_404(User, id=user_id)
+        user_from = self.request.user
+        return get_object_or_404(UserFollow, user_from=user_from, user_to=user_to)
+
