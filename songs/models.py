5fa2c87101bc3a70fe748a715b856c68d9833f2a
XXXDESHXXX <kirya.shvarts@list.ru>
2024-03-11 17:43:10 +0300

    Create UserSongLike model

--- songs/models.py
+++ songs/models.py
@@ -4,9 +4,18 @@ from genres.models import Genre
 from users.models import User
 
 
+class UserSongLike(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 class Song(models.Model):
     file = models.FileField()
     name = models.CharField(max_length=64)
+    likes = models.ManyToManyField(UserSongLike)
     genre = models.ForeignKey(Genre, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
 
