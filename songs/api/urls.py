7a6f31916a62a7fe193cebe2003f0d414439d4b1
XXXDESHXXX <kirya.shvarts@list.ru>
2024-03-18 12:23:49 +0300

    Create url for delete song like api; Refactor UserSongLikeCreate and Delete API views

--- songs/api/urls.py
+++ songs/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from songs.api.views import UserSongLikeCreateAPIView
+from songs.api.views import UserSongLikeCreateAPIView, UserSongLikeDeleteAPIView
 
 app_name = 'songs'
 
 urlpatterns = [
     path('songs/likes/create/', UserSongLikeCreateAPIView.as_view(), name='user_like_create'),
+    path('songs/likes/delete/<int:song_id>/', UserSongLikeDeleteAPIView.as_view(),
+         name='user_like_delete'),
 ]
--- songs/api/views.py
+++ songs/api/views.py
@@ -1,12 +1,38 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 
-from songs.api.serializers import UserSongLikeSerializer
+from songs.api.serializers import UserSongLikeSerializer, UserSongLikeCreateSerializer
+from songs.models import UserSongLike, Song
+from users.models import UserFollow, User
 
 
 class UserSongLikeCreateAPIView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSongLikeCreateSerializer
+
+    def perform_create(self, serializer: Serializer) -> None:
+        user = self.request.user
+
+        serializer.save(user=user)
+
+    def create(self, request, *args, **kwargs):
+        user = self.request.user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if UserSongLike.objects.filter(user=user, liked_song=serializer.validated_data["liked_song"]):
+            return Response({"detail": "You cannot like the same song twice"}, status=status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
+
+
+class UserSongLikeDeleteAPIView(DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = UserSongLikeSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def get_object(self) -> UserSongLike:
+        song_id = self.kwargs.get("song_id")
+        user = self.request.user
+        song = get_object_or_404(Song, id=song_id)
+        return get_object_or_404(UserSongLike, user=user, liked_song=song)
