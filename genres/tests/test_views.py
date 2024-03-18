5b454ae46a88b0f1b8a2cda5286423005a1e949e
XXXDESHXXX <kirya.shvarts@list.ru>
2024-03-18 13:26:01 +0300

    Create test for GenreListView

---
+++ genres/tests/test_views.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from genres.models import Genre
+from genres.api.serializers import GenreSerializer
+
+
+class GenreListViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('genres:api:genre_list')
+        self.genre1 = Genre.objects.create(name='Genre 1')
+        self.genre2 = Genre.objects.create(name='Genre 2')
+
+    def test_get_genre_list(self):
+        response = self.client.get(self.url)
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
