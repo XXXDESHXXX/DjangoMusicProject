b6d0e44dc3c66fe1d3cc4db7afd2b7c6a687e34b
XXXDESHXXX <kirya.shvarts@list.ru>
2024-03-18 13:27:21 +0300

    Create test for GenreSerializer

---
+++ genres/tests/test_serializers.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from genres.models import Genre
+from genres.api.serializers import GenreSerializer
+
+
+class GenreSerializerTest(TestCase):
+    def setUp(self):
+        self.genre = Genre.objects.create(name='Test Genre')
+        self.serializer = GenreSerializer(instance=self.genre)
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertEqual(set(data.keys()), ({'id', 'name'}))
+
+    def test_id_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data['id'], self.genre.id)
+
+    def test_name_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data['name'], 'Test Genre')
+
+    def test_create_valid_genre(self):
+        serializer = GenreSerializer(data={'name': 'Test Genre'})
+        self.assertTrue(serializer.is_valid())
