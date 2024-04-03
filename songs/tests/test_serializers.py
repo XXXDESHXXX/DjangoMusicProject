50dab19505c0f836912df4c7c78e6a8f5fc02971
XXXDESHXXX <kirya.shvarts@list.ru>
2024-04-03 10:10:14 +0300

    Create tests for UserSongLikeSerializer; UserSongLikeCreateAPIView, UserSongLikeDeleteAPIView

--- songs/tests/test_serializers.py
+++ songs/tests/test_serializers.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from users.models import User
 from songs.models import Song, UserSongLike
 from genres.models import Genre
-from songs.api.serializers import SongSerializer
+from songs.api.serializers import SongSerializer, UserSongLikeSerializer
 
 
 class SongSerializerTest(TestCase):
@@ -12,7 +12,6 @@ class SongSerializerTest(TestCase):
         song = Song.objects.create(
             name="Test Song", file="test_song.mp3", genre=genre, user=user
         )
-        UserSongLike.objects.create(user=user, liked_song=song)
 
     def test_song_serializer(self) -> None:
         song = Song.objects.first()
@@ -28,3 +27,36 @@ class SongSerializerTest(TestCase):
         serializer = SongSerializer()
         expected_fields = ["id", "name", "file", "likes", "genre", "user"]
         self.assertEqual(set(serializer.fields.keys()), set(expected_fields))
+
+
+class UserSongLikeSerializerTest(TestCase):
+    def setUp(self) -> None:
+        self.genre = Genre.objects.create(name="Jazz")
+        self.user = User.objects.create(username="test_user")
+        self.song = Song.objects.create(
+            name="Test Song", genre_id=self.genre.id, user=self.user
+        )
+        self.usersonglike_data = {
+            "user": self.user.id,
+            "liked_song": self.song.id,
+            "created_at": "2024-03-26T12:00:00Z",
+        }
+        self.serializer = UserSongLikeSerializer(data=self.usersonglike_data)
+
+    def test_valid_data(self) -> None:
+        self.assertTrue(self.serializer.is_valid())
+
+    def test_invalid_data_missing_fields(self) -> None:
+        invalid_data = {"created_at": "2024-03-26T12:00:00Z"}
+        serializer = UserSongLikeSerializer(data=invalid_data)
+        self.assertFalse(serializer.is_valid())
+
+    def test_user_field_content(self) -> None:
+        self.assertTrue(self.serializer.is_valid())
+        data = self.serializer.data
+        self.assertEqual(data["user"], self.usersonglike_data["user"])
+
+    def test_liked_song_field_content(self) -> None:
+        self.assertTrue(self.serializer.is_valid())
+        data = self.serializer.data
+        self.assertEqual(data["liked_song"], self.usersonglike_data["liked_song"])
--- songs/tests/test_views.py
+++ songs/tests/test_views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
 from genres.models import Genre
-from songs.models import Song
+from songs.models import Song, UserSongLike
 from users.models import User
 
 
@@ -39,3 +39,55 @@ class SongListAPIViewTest(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 0)
+
+
+class UserSongLikeCreateAPIViewTest(APITestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create(username="test_user")
+        self.genre = Genre.objects.create(name="Jazz")
+        self.song = Song.objects.create(
+            name="Test Song", genre_id=self.genre.id, user=self.user
+        )
+        self.client.force_authenticate(user=self.user)
+        self.url = reverse("songs:api:user_like_create")
+
+    def test_create_usersonglike(self) -> None:
+        data = {"liked_song": self.song.id}
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_duplicate_usersonglike(self) -> None:
+        UserSongLike.objects.create(user=self.user, liked_song=self.song)
+
+        data = {"liked_song": self.song.id}
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UserSongLikeDeleteAPIViewTest(APITestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create(username="test_user")
+        self.genre = Genre.objects.create(name="Jazz")
+        self.song = Song.objects.create(
+            name="Test Song", genre_id=self.genre.id, user=self.user
+        )
+        self.usersonglike = UserSongLike.objects.create(
+            user=self.user, liked_song=self.song
+        )
+        self.client.force_authenticate(user=self.user)
+        self.url = reverse(
+            "songs:api:user_like_delete", kwargs={"song_id": self.song.id}
+        )
+
+    def test_delete_usersonglike(self) -> None:
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(UserSongLike.objects.filter(id=self.usersonglike.id).exists())
+
+    def test_delete_nonexistent_usersonglike(self) -> None:
+        non_existent_song_id = self.song.id + 1
+        url = reverse(
+            "songs:api:user_like_delete", kwargs={"song_id": non_existent_song_id}
+        )
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
