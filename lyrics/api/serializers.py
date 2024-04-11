d8ebf10558147ee2fbf084c89f4f76410d340382
XXXDESHXXX <kirya.shvarts@list.ru>
2024-04-11 12:18:15 +0300

    Create LyricLineTimecodeSerializer

--- lyrics/api/serializers.py
+++ lyrics/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from lyrics.models import Lyric
+from lyrics.models import Lyric, LyricLineTimecode
 
 
 class LyricSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lyric
         fields = ("language", "song_id")
+
+
+class LyricLineTimecodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LyricLineTimecode
+        fields = ("timecode", "text_line")
