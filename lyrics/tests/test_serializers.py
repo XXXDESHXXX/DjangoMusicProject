56102418f8ac7836c3c5cf4486c0a5243a022a4d
XXXDESHXXX <kirya.shvarts@list.ru>
2024-04-13 18:29:14 +0300

    Create test for LyricLineTimecodeSerializer

--- lyrics/tests/test_serializers.py
+++ lyrics/tests/test_serializers.py
@@ -1,8 +1,11 @@
+from datetime import datetime, timedelta
+
 from django.test import TestCase
+from django.utils import timezone
 
 from genres.models import Genre
-from lyrics.models import Lyric
-from lyrics.api.serializers import LyricSerializer
+from lyrics.models import Lyric, LyricLineTimecode
+from lyrics.api.serializers import LyricSerializer, LyricLineTimecodeSerializer
 from songs.models import Song
 from users.models import User
 
@@ -35,4 +38,39 @@ class LyricSerializerTestCase(TestCase):
         self.assertFalse(serializer_with_empty_data.is_valid())
 
         serializer_with_valid_data = LyricSerializer(data=self.lyric_data)
+        self.assertTrue(serializer_with_valid_data.is_valid())
+
+
+class LyricLineTimecodeSerializerTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="Test1 user")
+        self.genre = Genre.objects.create(name="Test Genre")
+        self.song = Song.objects.create(
+            name="Test Song", genre_id=self.genre.id, user_id=self.user.id
+        )
+        self.lyric = Lyric.objects.create(language="RU", song=self.song)
+        self.timecode = timedelta(hours=3, minutes=3, seconds=3)
+        self.text_line = "Some text line"
+        self.lyric_line_timecode = LyricLineTimecode.objects.create(
+            lyric_id=self.lyric.id, timecode=self.timecode, text_line=self.text_line
+        )
+
+    def test_serializer_data(self):
+        serializer = LyricLineTimecodeSerializer(instance=self.lyric_line_timecode)
+        expected_data = {
+            "timecode": "03:03:03",
+            "text_line": self.lyric_line_timecode.text_line,
+        }
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_serializer_validation(self):
+        invalid_data = {"timecode": "invalid_timecode_format", "text_line": ""}
+        serializer_with_invalid_data = LyricLineTimecodeSerializer(data=invalid_data)
+        self.assertFalse(serializer_with_invalid_data.is_valid())
+
+        valid_data = {
+            "timecode": timedelta(days=3, hours=3, minutes=3),
+            "text_line": "New text line",
+        }
+        serializer_with_valid_data = LyricLineTimecodeSerializer(data=valid_data)
         self.assertTrue(serializer_with_valid_data.is_valid())
