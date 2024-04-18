99e7348b00977d6d9115b2bda8533e4abb941017
XXXDESHXXX <kirya.shvarts@list.ru>
2024-04-18 11:16:09 +0300

    Add media urls

--- core/settings.py
+++ core/settings.py
@@ -21,6 +21,9 @@ load_dotenv("./.env")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -26,3 +28,8 @@ urlpatterns = [
     path("playlists/", include("playlists.urls", namespace="playlists")),
     path("lyrics/", include("lyrics.urls", namespace="lyrics")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
