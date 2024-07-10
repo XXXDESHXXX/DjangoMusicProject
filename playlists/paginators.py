dc0b539df8369dd3ad43d4684ce8da87a717669f
XXXDESHXXX <kirya.shvarts@list.ru>
2024-07-10 20:52:36 +0300

    Create constant and paginator for UserPlaylistSongListAPIView

--- playlists/paginators.py
+++ playlists/paginators.py
@@ -1,7 +1,12 @@
 from rest_framework.pagination import LimitOffsetPagination
 
-from playlists.constants import USER_PLAYLIST_LIST_PAGINATE_BY
+from playlists.constants import USER_PLAYLIST_LIST_PAGINATE_BY, USER_PLAYLIST_SONG_LIST_PAGINATE_BY
 
 
 class UserPlaylistPageLimitOffsetPagination(LimitOffsetPagination):
     default_limit = USER_PLAYLIST_LIST_PAGINATE_BY
+
+
+class UserPlaylistSongPageLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = USER_PLAYLIST_SONG_LIST_PAGINATE_BY
+
