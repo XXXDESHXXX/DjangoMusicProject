8f8e7286726f20357e51611821ede65b03f35e3e
XXXDESHXXX <kirya.shvarts@list.ru>
2024-07-10 22:07:47 +0300

    Refactoring

--- lyrics/api/urls.py
+++ lyrics/api/urls.py
@@ -12,29 +12,29 @@ from lyrics.api.views import (
 app_name = "lyrics"
 
 urlpatterns = [
-    path("lyrics/<int:song_id>", LyricSongAPIView.as_view(), name="lyric_song_list"),
+    path("song/<int:song_id>/", LyricSongAPIView.as_view(), name="lyric_song_list"),
     path(
-        "lyrics/create_lyric/<int:song_id>",
+        "create/<int:song_id>/",
         LyricCreateAPIView.as_view(),
         name="create_lyric",
     ),
     path(
-        "lyrics/delete_lyric/<int:lyric_id>",
+        "delete/<int:lyric_id>/",
         LyricDeleteAPIView.as_view(),
         name="delete_lyric",
     ),
     path(
-        "lyrics/<int:lyric_id>/lyric_line_timecodes",
+        "lyric_line_timecodes/<int:lyric_id>/",
         LyricLineTimecodeListAPIView.as_view(),
         name="lyric_line_timecode_list",
     ),
     path(
-        "lyrics/delete_lyric_line_timecode/<int:lyric_line_timecode_id>",
+        "lyric_line_timecode/delete/<int:lyric_line_timecode_id>/",
         LyricLineTimecodeDeleteAPIView.as_view(),
         name="delete_lyric_line_timecode",
     ),
     path(
-        "lyrics/create_lyric_line_timecode/<int:lyric_id>",
+        "lyric_line_timecode/create/<int:lyric_id>/",
         LyricLineTimecodeCreateAPIView.as_view(),
         name="create_lyric_line_timecode",
     ),
