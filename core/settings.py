c48b4002b6580d810c9d9667362ba0bde7f0b8ce
XXXDESHXXX <kirya.shvarts@list.ru>
2024-07-11 12:58:39 +0300

    Refactoring

--- core/settings.py
+++ core/settings.py
@@ -45,13 +45,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
     "genres",
     "actions",
     "lyrics",
     "playlists",
     "songs",
     "users",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
