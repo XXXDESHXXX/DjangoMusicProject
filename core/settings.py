9c1a21e6ba7b996760b3ddb3c17c71003cc65b3e
XXXDESHXXX <kirya.shvarts@list.ru>
2024-07-11 13:48:10 +0300

    Refactoring

--- core/settings.py
+++ core/settings.py
@@ -136,9 +136,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 if DEBUG:
     STATICFILES_DIRS = ()
 
